f66217d3103efdaf4894e2145b0b3fb7859a3fa3
progbasma <prog.basma@gmail.com>
2025-09-19 11:52:10 +0300

    styles in react component

--- src/assets/components/navbar/Navbar.jsx
+++ src/assets/components/navbar/Navbar.jsx
@@ -1,44 +1,53 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import './Navbar.css'
+// import './Navbar.module.css'
+import './Navbar.scss'
+import styled from 'styled-components'
+
+const Navstyledcomponent = styled.nav`
+  background-color: #333;
+  padding: 10px;
+  text-align: center;
+`;
+
+
+
+// const styles={
+//   navstyle:{
+//      background-color: #333;
+//  padding: 10px;
+//  text-align: center;
+//   },
+//   ulstyle:{
+//     listStyle:"none",
+//     display:"flex"
+//   }
+// }
+
 
 function Navbarc() {
+  /*
+  ways to style react.js component 
+  1- inline css
+  2- css external file and import it in jsx file 
+  3- css modules or scss modules
+  4- css processors : scss , sass 
+  5- styled components  or emotion
+  6- css framework like bootstrap or tailwind css
+  7- react.js ui components library like shadcn ui , material ui ,  chakra ui
+  
+  
+  */
   return (
-    <>
-  <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-  </Navbar>
-  <Container className='mt-5 p-md-5'>
-    <Row>
-      <Col md={4}>1 of 3</Col>
-      <Col className='bg-primary text-light' md={4}>2 of 3</Col>
-      <Col md={4}>3 of 3</Col>
-    </Row>
-  </Container>
+  <>
 
+    <Navstyledcomponent  as="nav">
+      <ul>
+        <li><a href="/">home page</a></li>
+        <li><a href="/">about us</a></li>
+        <li><a href="/">contact us</a></li>
+      </ul>
+    </Navstyledcomponent>
   </>
 
   )
